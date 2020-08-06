cf08f6cd128deea9412a630b06f17060ac06392b
KingRealzYT <41351144+KingRealzYT@users.noreply.github.com>
2020-08-05 19:42:51 -0500

    Shrek Texture

--- src/main/java/com/kingrealzyt/swords/client/model/ShrekModel.java
+++ src/main/java/com/kingrealzyt/swords/client/model/ShrekModel.java
@@ -5,94 +5,88 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
-import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
 
-public class ShrekModel<T extends ShrekEntity> extends EntityModel {
+public class ShrekModel<T extends ShrekEntity> extends EntityModel<T> {
 
+    private final ModelRenderer Waist;
     private final ModelRenderer RightLeg;
-    private final ModelRenderer bone3;
-    private final ModelRenderer bone2;
-    private final ModelRenderer Torso;
     private final ModelRenderer LeftLeg;
-    private final ModelRenderer bone7;
-    private final ModelRenderer bone8;
-    private final ModelRenderer RightArm;
-    private final ModelRenderer LeftArm;
+    private final ModelRenderer Chest;
+    private final ModelRenderer LeftHand;
+    private final ModelRenderer RightHand;
     private final ModelRenderer Head;
+    private final ModelRenderer RightEar;
+    private final ModelRenderer LeftEar;
 
     public ShrekModel() {
-        textureWidth = 16;
-        textureHeight = 16;
+        textureWidth = 60;
+        textureHeight = 60;
+
+        Waist = new ModelRenderer(this);
+        Waist.setRotationPoint(4.0F, 24.0F, 0.0F);
+        Waist.setTextureOffset(28, 44).addBox(-11.0F, -12.0F, -1.0F, 12.0F, 2.0F, 3.0F, 0.0F, false);
 
         RightLeg = new ModelRenderer(this);
-        RightLeg.setRotationPoint(-4.0F, 20.0F, 0.0F);
-
-
-        bone3 = new ModelRenderer(this);
-        bone3.setRotationPoint(0.0F, -7.0F, 0.0F);
-        RightLeg.addChild(bone3);
-        bone3.setTextureOffset(0, 0).addBox(-2.0F, 0.0F, -1.0F, 5.0F, 11.0F, 3.0F, 0.0F, false);
-
-        bone2 = new ModelRenderer(this);
-        bone2.setRotationPoint(0.0F, -7.0F, 0.0F);
-        RightLeg.addChild(bone2);
-        bone2.setTextureOffset(0, 0).addBox(-3.0F, 0.0F, -2.0F, 7.0F, 6.0F, 5.0F, 0.0F, false);
-
-        Torso = new ModelRenderer(this);
-        Torso.setRotationPoint(0.0F, 9.5F, 0.5F);
-        Torso.setTextureOffset(0, 0).addBox(-9.0F, -18.5F, -5.5F, 18.0F, 22.0F, 11.0F, 0.0F, false);
+        RightLeg.setRotationPoint(0.0F, 24.0F, 0.0F);
+        RightLeg.setTextureOffset(0, 47).addBox(-7.0F, -10.0F, -1.0F, 3.0F, 10.0F, 3.0F, 0.0F, false);
 
         LeftLeg = new ModelRenderer(this);
-        LeftLeg.setRotationPoint(3.0F, 20.0F, 0.0F);
+        LeftLeg.setRotationPoint(0.0F, 24.0F, 0.0F);
+        LeftLeg.setTextureOffset(16, 49).addBox(2.0F, -10.0F, 0.0F, 3.0F, 10.0F, 2.0F, 0.0F, false);
 
+        Chest = new ModelRenderer(this);
+        Chest.setRotationPoint(0.0F, 24.0F, 0.0F);
+        setRotationAngle(Chest, 0.0F, 0.0F, 0.0F);
+        Chest.setTextureOffset(1, 16).addBox(-7.0F, -24.0F, -1.0F, 12.0F, 12.0F, 3.0F, 0.0F, false);
 
-        bone7 = new ModelRenderer(this);
-        bone7.setRotationPoint(0.0F, -7.0F, 0.0F);
-        LeftLeg.addChild(bone7);
-        bone7.setTextureOffset(0, 0).addBox(-2.0F, 0.0F, -1.0F, 5.0F, 11.0F, 3.0F, 0.0F, false);
+        LeftHand = new ModelRenderer(this);
+        LeftHand.setRotationPoint(0.0F, -12.0F, 0.0F);
+        Chest.addChild(LeftHand);
+        LeftHand.setTextureOffset(14, 32).addBox(5.0F, -12.0F, -1.0F, 3.0F, 10.0F, 3.0F, 0.0F, false);
 
-        bone8 = new ModelRenderer(this);
-        bone8.setRotationPoint(0.0F, -7.0F, 0.0F);
-        LeftLeg.addChild(bone8);
-        bone8.setTextureOffset(0, 0).addBox(-3.0F, 0.0F, -2.0F, 7.0F, 6.0F, 5.0F, 0.0F, false);
-
-        RightArm = new ModelRenderer(this);
-        RightArm.setRotationPoint(12.0F, -3.0F, 0.0F);
-        RightArm.setTextureOffset(0, 0).addBox(-3.0F, -4.0F, -3.0F, 6.0F, 16.0F, 6.0F, 0.0F, false);
-        RightArm.setTextureOffset(0, 0).addBox(-4.0F, -5.0F, -4.0F, 8.0F, 9.0F, 8.0F, 0.0F, false);
-
-        LeftArm = new ModelRenderer(this);
-        LeftArm.setRotationPoint(-12.0F, -3.0F, 0.0F);
-        LeftArm.setTextureOffset(0, 0).addBox(-3.0F, -4.0F, -3.0F, 6.0F, 16.0F, 6.0F, 0.0F, false);
-        LeftArm.setTextureOffset(0, 0).addBox(-4.0F, -5.0F, -4.0F, 8.0F, 9.0F, 8.0F, 0.0F, false);
+        RightHand = new ModelRenderer(this);
+        RightHand.setRotationPoint(0.0F, 12.0F, 0.0F);
+        setRotationAngle(RightHand, 0.0F, 0.0F, 0.0F);
+        RightHand.setTextureOffset(0, 32).addBox(-10.0F, -12.0F, -1.0F, 3.0F, 10.0F, 3.0F, 0.0F, false);
 
         Head = new ModelRenderer(this);
-        Head.setRotationPoint(0.0F, 24.0F, 0.0F);
-        Head.setTextureOffset(0, 0).addBox(-6.0F, -45.0F, -6.0F, 12.0F, 12.0F, 13.0F, 0.0F, false);
+        Head.setRotationPoint(-8.0F, -11.0F, 0.0F);
+        RightHand.addChild(Head);
+        Head.setTextureOffset(1, 1).addBox(3.0F, -8.0F, -3.0F, 8.0F, 7.0F, 7.0F, 0.0F, false);
+
+        RightEar = new ModelRenderer(this);
+        RightEar.setRotationPoint(0.0F, 24.0F, 0.0F);
+        RightEar.setTextureOffset(30, 3).addBox(-7.0F, -31.0F, -1.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+        RightEar.setTextureOffset(37, 2).addBox(-9.0F, -32.0F, -1.0F, 3.0F, 2.0F, 1.0F, 0.0F, false);
+
+        LeftEar = new ModelRenderer(this);
+        LeftEar.setRotationPoint(24.0F, 24.0F, 0.0F);
+        setRotationAngle(LeftEar, 0.0F, 3.1416F, 0.0F);
+        LeftEar.setTextureOffset(32, 8).addBox(19.0F, -31.0F, -1.0F, 2.0F, 2.0F, 1.0F, 0.0F, false);
+        LeftEar.setTextureOffset(38, 8).addBox(17.0F, -32.0F, -1.0F, 3.0F, 2.0F, 1.0F, 0.0F, false);
     }
 
     @Override
-    public void setRotationAngles(Entity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+    public void setRotationAngles(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
         this.Head.rotateAngleX = headPitch * ((float)Math.PI / 180F);
-        this.RightArm.rotateAngleY = netHeadYaw * ((float)Math.PI / 180F);
-        this.LeftArm.rotateAngleX = ((float)Math.PI / 2F);
-        this.bone2.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-        this.bone3.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
-        this.RightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.Head.rotateAngleY = netHeadYaw * ((float)Math.PI / 180F);
+        this.Chest.rotateAngleX = ((float)Math.PI / 2F);
         this.LeftLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-        this.bone7.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-        this.bone8.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-        }
+        this.RightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.RightHand.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F + (float)Math.PI) * 1.4F * limbSwingAmount;
+        this.LeftHand.rotateAngleX = MathHelper.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
+    }
 
     @Override
     public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+        Waist.render(matrixStack, buffer, packedLight, packedOverlay);
         RightLeg.render(matrixStack, buffer, packedLight, packedOverlay);
-        Torso.render(matrixStack, buffer, packedLight, packedOverlay);
         LeftLeg.render(matrixStack, buffer, packedLight, packedOverlay);
-        RightArm.render(matrixStack, buffer, packedLight, packedOverlay);
-        LeftArm.render(matrixStack, buffer, packedLight, packedOverlay);
-        Head.render(matrixStack, buffer, packedLight, packedOverlay);
+        Chest.render(matrixStack, buffer, packedLight, packedOverlay);
+        RightHand.render(matrixStack, buffer, packedLight, packedOverlay);
+        RightEar.render(matrixStack, buffer, packedLight, packedOverlay);
+        LeftEar.render(matrixStack, buffer, packedLight, packedOverlay);
     }
 
     public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
--- src/main/java/com/kingrealzyt/swords/client/render/ShrekRenderer.java
+++ src/main/java/com/kingrealzyt/swords/client/render/ShrekRenderer.java
@@ -1,5 +1,6 @@
 package com.kingrealzyt.swords.client.render;
 
+import com.kingrealzyt.swords.Swords;
 import com.kingrealzyt.swords.client.model.ShrekModel;
 import com.kingrealzyt.swords.entities.ShrekEntity;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
@@ -8,13 +9,16 @@ import net.minecraft.util.ResourceLocation;
 
 public class ShrekRenderer extends MobRenderer<ShrekEntity, ShrekModel<ShrekEntity>> {
 
+    protected static final ResourceLocation TEXTURE = new ResourceLocation(Swords.MOD_ID, "textures/entity/ShrekEntity");
 
     public ShrekRenderer(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new ShrekModel<>(), 1.0F);
+        super(renderManagerIn, new ShrekModel<>(), 1.3F);
     }
 
     @Override
     public ResourceLocation getEntityTexture(ShrekEntity entity) {
         return null;
     }
+
+
 }
